7112a94595c180a9c4b274d43a6bd95531a3c830
Squaad6z <carneiromoreirasteven22@gmail.com>
2025-05-10 15:48:53 +0200

    Modification Next config

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,20 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)", // applique la règle à toutes les pages
+        source: "/(.*)", // toutes les routes
         headers: [
           {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' https://fonts.gstatic.com;
               script-src 'self' 'unsafe-inline';
               frame-src 'self' https://www.google.com https://maps.google.com;
               connect-src 'self';
               img-src 'self' data: https:;
-            `.replace(/\s{2,}/g, " ").trim(), // compactage de l'en-tête
+            `.replace(/\s{2,}/g, " ").trim(),
           },
         ],
       },
